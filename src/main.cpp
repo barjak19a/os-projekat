b13a8a788a9ab5b9feac13200c8a5b22b3e4ad46
barjak19a <nbarjakt@gmail.com>
2023-11-20 11:35:05 -0800

    Popravio do thread_dispathc()

--- src/main.cpp
+++ src/main.cpp
@@ -25,15 +25,17 @@ int main() {
 
     _thread::running = _thread::thread_create(nullptr, nullptr, nullptr);
 
-    riscv::ms_sstatus(riscv::SSTATUS_SPIE | riscv::SSTATUS_SPP);
+    // ovako je nama ne znamo zasto riscv::ms_sstatus(riscv::SSTATUS_SPIE | riscv::SSTATUS_SPP);
+    // ovako je lab ne razumemo zasto
+    riscv::ms_sstatus(riscv::SSTATUS_SIE);
     // upisuje 45 i poziva syscall 45 - getPid()
     __asm__ volatile ("mv a0, %0" : : "r" (45));
     __asm__ volatile ("ecall");
     
     thread_t handle = nullptr;
     thread_create(&handle, testMethod, nullptr);
-    __asm__ volatile ("csrw sepc, %0" : : "r" (0));
-    __asm__ volatile ("csrw sstatus, %0" : : "r" (0x120));
+    //__asm__ volatile ("csrw sepc, %0" : : "r" (0));
+    //__asm__ volatile ("csrw sstatus, %0" : : "r" (0x120));
     thread_dispatch();
     return 0;
 }
